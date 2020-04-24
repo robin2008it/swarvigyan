097a98ceeefaf9233f806dd815f7dbcc70bc5302
Robin Garg <GargRo@india.rapidigm.com>
2020-04-24 21:09:08 +0530

    Updates

---
+++ precache-manifest.000668b1163c9cc8e3f25b45d9682236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39cd6b23e103f15b6536b7a9be47523",
+    "url": "/swarvigyan/index.html"
+  },
+  {
+    "revision": "56548ca1c80fdf54dc42",
+    "url": "/swarvigyan/static/css/main.e481f7f5.chunk.css"
+  },
+  {
+    "revision": "8f16f85a8258c475def4",
+    "url": "/swarvigyan/static/js/2.5b977ca6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/swarvigyan/static/js/2.5b977ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56548ca1c80fdf54dc42",
+    "url": "/swarvigyan/static/js/main.e95271da.chunk.js"
+  },
+  {
+    "revision": "0ac1a7b46f013f555bed",
+    "url": "/swarvigyan/static/js/runtime-main.a3360440.js"
+  }
+]);
