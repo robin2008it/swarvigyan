da3ce668e00b9856d155dff5cae16b22c183ecad
Robin Garg <GargRo@india.rapidigm.com>
2020-04-25 17:36:31 +0530

    Updates

---
+++ precache-manifest.6a28746505c45b726ee0abb7ffdd3867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ea188dd2e93112234d5280f2a9991e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74611240b1ded0669052",
+    "url": "/static/css/main.e481f7f5.chunk.css"
+  },
+  {
+    "revision": "136b2873386074f6c04f",
+    "url": "/static/js/2.e85d2aaa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e85d2aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74611240b1ded0669052",
+    "url": "/static/js/main.7b95cb28.chunk.js"
+  },
+  {
+    "revision": "8b56923d3b4841ae892b",
+    "url": "/static/js/runtime-main.53a29903.js"
+  }
+]);
