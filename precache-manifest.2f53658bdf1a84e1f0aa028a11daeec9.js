ade8271a40b535314f07a5b0f43a2ba164068e04
Robin Garg <GargRo@india.rapidigm.com>
2020-04-25 17:11:34 +0530

    Updates

---
+++ precache-manifest.2f53658bdf1a84e1f0aa028a11daeec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f5f95149b114174b200f41d004aede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20f5d1c609c0762a2eb",
+    "url": "/static/css/main.e481f7f5.chunk.css"
+  },
+  {
+    "revision": "93f697a48d42f5503056",
+    "url": "/static/js/2.514f56b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.514f56b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a20f5d1c609c0762a2eb",
+    "url": "/static/js/main.23cf08a5.chunk.js"
+  },
+  {
+    "revision": "8b56923d3b4841ae892b",
+    "url": "/static/js/runtime-main.53a29903.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swarvigyan/precache-manifest.c9eb181169de4afd23496f196c05e1be.js"
+  "/precache-manifest.2f53658bdf1a84e1f0aa028a11daeec9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swarvigyan/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
